ec9d5f795c83f7f039aac918ac5f5d00fe48f3dd
safroniv <safroniv@mail.ru>
2022-06-21 21:13:24 +0300

    GB Урок 8 - базовая структура

--- JobLessonOOP08v04Part01/Program.cs
+++ JobLessonOOP08v04Part01/Program.cs
@@ -1,1 +1,26 @@
 ﻿Console.WriteLine("Hello, World!");
+
+
+
+
+public class ObjectDirectory : IObjectFileSystem
+{
+
+}
+public class ObjectFile : IObjectFileSystem
+{
+
+}
+public interface IObjectFileSystem
+{
+    //Требует реализации по CRUD (Create Read Update Delete)
+    //но с учётом переноса и копирования:
+    //Read Create Delete Rename Copy Transfer - RCDRCT
+
+}
+
+public class ObjectFileSystem
+{
+    public string ObjectName { get; set; }
+    public string ObjectPath { get; set; }
+}
